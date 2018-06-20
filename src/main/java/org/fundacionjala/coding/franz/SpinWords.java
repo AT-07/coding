4c816076f4c6e4fee4e8cdb227a65e76cfce325b
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-19 23:59:20 -0400

    franz SpinWords

---
+++ src/main/java/org/fundacionjala/coding/franz/SpinWords.java
@@ -0,0 +1,42 @@
+package org.fundacionjala.coding.franz;
+
+/**
+ * This is a class for Spin words.
+ */
+public class SpinWords {
+    private static final int LIMIT_TAM = 4;
+
+    /**
+     * this method is a spin words.
+     *
+     * @param sentence is the words that use for excercise
+     * @return words reverse if is a length major to 5 for word
+     */
+    public String spinWords(String sentence) {
+
+        String[] words = sentence.split(" ");
+        StringBuilder wordComplete = new StringBuilder();
+
+        if (words.length != 1) {
+            wordComplete.append(reverseWord(words[0])).append(" ");
+        } else {
+            wordComplete.append(reverseWord(words[0]));
+        }
+        for (int i = 1; i < words.length; i++) {
+            wordComplete.append(reverseWord(words[i])).append(" ").toString();
+        }
+        return wordComplete.toString().trim();
+    }
+
+    /**
+     * this method reverse a word.
+     *
+     * @param word is a word that need reverse
+     * @return the word reverse
+     */
+    public String reverseWord(String word) {
+        String wordRev = word.length() > LIMIT_TAM ? new StringBuilder(word).reverse().toString()
+                : new StringBuilder(word).toString();
+        return wordRev;
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/franz/SpinWordsTest.java
@@ -0,0 +1,32 @@
+package org.fundacionjala.coding.franz;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * this class is a test of class SpinWord.
+ */
+public class SpinWordsTest {
+    private SpinWords spin;
+
+    /**
+     * this method initialize the object SpinWords.
+     */
+    @Before
+    public void ini() {
+        spin = new SpinWords();
+    }
+
+    /**
+     * this is a test for a method.
+     */
+    @Test
+    public void testSpinWord() {
+        assertEquals("emocleW", spin.spinWords("Welcome"));
+        assertEquals("Hey wollef sroirraw", spin.spinWords("Hey fellow warriors"));
+        assertEquals("is a tes", spin.spinWords("is a tes"));
+        assertEquals("ylsuoireS", spin.spinWords("Seriously"));
+    }
+}
