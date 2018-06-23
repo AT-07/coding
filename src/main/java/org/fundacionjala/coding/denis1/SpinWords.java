fb758b9803ddbd0bb297c392076cfbd6a6b86b14
Denis <denis_cc1729@hotmail.com>
2018-06-22 20:16:20 -0400

    denis SpinWords

---
+++ src/main/java/org/fundacionjala/coding/denis1/SpinWords.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.coding.denis1;
+
+/**
+ * class SpinWords.
+ */
+public class SpinWords {
+    private static final int ZISEWORD = 5;
+
+    /**
+     * invest of the words.
+     *
+     * @param sentence OF STRING INVEST.
+     * @return string.
+     */
+    public String stopSpinWords(String sentence) {
+
+        String wordInvertido = "";
+        String[] wordsParts = sentence.split(" ");
+        StringBuilder word = new StringBuilder();
+
+        for (int i = 0; i < wordsParts.length; i++) {
+            word.append(wordsParts[i]);
+            wordInvertido += word.length() < ZISEWORD ? word.append(" ").toString() : word.reverse().append(" ").toString();
+            word.delete(0, word.length());
+        }
+
+        return wordInvertido.trim();
+
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/denis1/TestSpinWords.java
@@ -0,0 +1,53 @@
+package org.fundacionjala.coding.denis1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * unit test for the class TestSpinWords.
+ */
+
+public class TestSpinWords {
+    private SpinWords spin;
+    private String value;
+    private String value1;
+    private String valueFail;
+    private String valueFail1;
+
+    /**
+     * Init of variables.
+     */
+    @Before
+    public void before() {
+        spin = new SpinWords();
+        value = "Welcome";
+        value1 = "Hey fellow warriors";
+        valueFail = "Hey fellow warriors";
+        valueFail1 = "hi";
+    }
+
+
+    /**
+     * test for validation of testSpinWordsSpinWordsPass.
+     */
+
+    @Test
+    public void testSpinWordsSpinWordsPass() {
+        assertEquals("emocleW", spin.stopSpinWords(value));
+        assertEquals("Hey wollef sroirraw", spin.stopSpinWords(value1));
+    }
+
+    /**
+     * test for validation of testSpinWordsSpinWordsFails.
+     */
+
+    @Test
+    public void testSpinWordsSpinWordsFails() {
+        assertNotEquals("Hey wolle sroirraw", spin.stopSpinWords(valueFail));
+        assertNotEquals("ih", spin.stopSpinWords(valueFail1));
+    }
+
+}
