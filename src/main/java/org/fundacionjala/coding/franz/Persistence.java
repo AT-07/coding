b74ada03275575586aaa009695362d464d8d2189
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-22 23:08:39 -0400

    franz persistence code

---
+++ src/main/java/org/fundacionjala/coding/franz/Persistence.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.coding.franz;
+
+/**
+ * this is a class Persistance.
+ */
+public class Persistence {
+
+    public static final int NUMBER_TEN = 10;
+    /**
+     * @param n is a number for evalua its persistence
+     * @return the cant of persistence of param
+     */
+    public int persistence(long n) {
+        int result = 0;
+        long multiple = 1;
+        while (String.valueOf(n).length() > 1) {
+            multiple = 1;
+            while (n != 0) {
+                multiple *= n % NUMBER_TEN;
+                n = n / NUMBER_TEN;
+            }
+            result++;
+            n = multiple;
+
+        }
+        return result;
+    }
+}
