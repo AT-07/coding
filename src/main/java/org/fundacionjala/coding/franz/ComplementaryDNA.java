24bcd1cae6c524210e2d7fe6712e07332eb298a5
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-25 12:29:37 -0400

    franz ComplementaryDNA code

---
+++ src/main/java/org/fundacionjala/coding/franz/ComplementaryDNA.java
@@ -0,0 +1,24 @@
+package org.fundacionjala.coding.franz;
+
+/**
+ * this class is a DNA.
+ */
+public class ComplementaryDNA {
+    private static final String A = "A";
+    private static final String T = "T";
+    private static final String SPACE = " ";
+    private static final String C = "C";
+    private static final String G = "G";
+
+    /**
+     * this method is about make complement.
+     *
+     * @param dna is a Stirng that make complement
+     * @return a String complement
+     */
+    public String makeComplement(String dna) {
+
+        return dna.replaceAll(A, SPACE).replaceAll(T, A).replaceAll(SPACE, T)
+                .replaceAll(C, SPACE).replaceAll(G, C).replaceAll(SPACE, G);
+    }
+}
