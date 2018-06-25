64aca58a74dda7d7672c0ee8e9a5b96c12ce10df
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-25 12:57:29 -0400

    refactor franz eanCode

--- src/main/java/org/fundacionjala/coding/franz/EanCode.java
+++ src/main/java/org/fundacionjala/coding/franz/EanCode.java
@@ -15,14 +15,12 @@ public class EanCode {
      */
     public boolean validationEAN(String number) {
         int sum = 0;
-        int eanNumber;
         for (int i = 0; i < number.length() - 1; i++) {
-            eanNumber = i + 1;
-            sum += eanNumber % 2 == 0 ? Character.getNumericValue(number.charAt(i)) * TRHEE
-                    : Character.getNumericValue(number.charAt(i));
+            int numericValue = Character.getNumericValue(number.charAt(i));
+            sum += (i + 1) % 2 == 0 ? numericValue * TRHEE : numericValue;
         }
         int endDigit = Character.getNumericValue(number.charAt(number.length() - 1));
-        sum = sum % MOD == 0 ? 0 : MOD - (sum % MOD);
-        return endDigit == sum;
+        int checkSum = sum % MOD == 0 ? 0 : MOD - (sum % MOD);
+        return endDigit == checkSum;
     }
 }
