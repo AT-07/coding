1958590fdd41f1649c0c2a53c5a90ac8846e119b
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-26 17:20:10 -0400

    modify method and create static variable

--- src/main/java/org/fundacionjala/coding/franz/Twisted.java
+++ src/main/java/org/fundacionjala/coding/franz/Twisted.java
@@ -6,24 +6,21 @@ import java.util.Arrays;
  * this is a class Twisted.
  */
 public class Twisted {
+    private static final char THREE = '3';
+    private static final char FLAG = 't';
+    private static final char SEVEN = '7';
     /**
      * this method os a sort twisted.
+     *
      * @param array numers that sort in twisted
      * @return arrays number sorted
      */
     public Integer[] sortTwisted(Integer[] array) {
         return Arrays.stream(array)
-                .map(number ->
-                    Integer.valueOf(number.toString()
-                            .replace('3', 't')
-                            .replace('7', '3').replace('t', '7'))
-                )
-                .sorted()
-                .map(number ->
-                    Integer.valueOf(number.toString()
-                            .replace('3', 't')
-                            .replace('7', '3').replace('t', '7'))
-                )
-                .toArray(Integer[]::new);
+                .map(number -> Integer.valueOf(number.toString()
+                        .replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
+                ).sorted().map(number -> Integer.valueOf(number.toString()
+                        .replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
+                ).toArray(Integer[]::new);
     }
 }
