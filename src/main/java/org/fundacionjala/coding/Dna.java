531d0f105994bde23593dda35d0e323401534cd6
cesar1414 <40204151+cesar1414@users.noreply.github.com>
2018-06-26 01:08:27 -0600

    CESAR DNA (#36)
    * CESAR DNA
    
    * CESAR DNA

---
+++ src/main/java/org/fundacionjala/coding/Dna.java
@@ -0,0 +1,17 @@
+package org.fundacionjala.coding;
+
+/**
+ *
+ * @author admin-hp
+ */
+public class Dna {
+
+    /**
+     *
+     * @param dna ingresa el dna .
+     * @return retorna el dna que se remplazo pero sin afectar a la cadena principal.
+     */
+    public String makeComplemet(String dna) {
+        return dna.replace("A", "t").replace("T", "a").replace("C", "g").replace("G", "c").toUpperCase();
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/DnaTest.java
@@ -0,0 +1,48 @@
+package org.fundacionjala.coding;
+
+/**
+ *
+ * @author admin-hp
+ */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ *
+ * @author admin-hp
+ */
+public class DnaTest {
+    private Dna dna;
+
+    /**
+     * iniciando el before.
+     */
+    @Before
+    public void before() {
+        dna = new Dna();
+
+    }
+
+    /**
+     * prueba de la cadena ATTGC.
+     */
+    @Test
+    public void testMakeComplemetCadena() {
+        String prueba = "ATTGC";
+        assertEquals("TAACG", dna.makeComplemet(prueba));
+    }
+
+    /**
+     * prueba de la cadena GTAT.
+     */
+    @Test
+    public void testMakeComplemetCadenaCorta() {
+        String prueba = "GTAT";
+        assertEquals("CATA", dna.makeComplemet(prueba));
+    }
+
+
+}
