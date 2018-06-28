0d87fb731a69a23457f5d2535db27a6e81103f40
erikAlberto <Erik.Vargas@fundacon-jala.org>
2018-06-27 20:06:23 -0400

    primer commit persistent bugger

---
+++ src/main/java/org/fundacionjala/coding/erik/Persistence.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.coding.erik;
+
+/**
+ * This is class is for persistence Bugger.
+ */
+public class Persistence {
+    private static final int DIVISOR = 10;
+
+    /**
+     * This is a method for number persistence.
+     *
+     * @param num is the number persistent.
+     * @return the number of multiplications.
+     */
+    public int numberPersistence(int num) {
+        int cont = 0;
+        while (num > DIVISOR) {
+            int multiplication = 1;
+            while (num != 0) {
+                multiplication = multiplication * (num % DIVISOR);
+                num = num / DIVISOR;
+            }
+            cont++;
+            num = multiplication;
+        }
+        return cont;
+    }
+}
