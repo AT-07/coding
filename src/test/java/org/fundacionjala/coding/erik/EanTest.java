ea0bcddfb2ea7904b7034ac7788ed979ed3fb2e3
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2018-06-28 19:47:23 -0400

    Clean code

--- src/test/java/org/fundacionjala/coding/erik/EanTest.java
+++ src/test/java/org/fundacionjala/coding/erik/EanTest.java
@@ -1,21 +1,21 @@
-package org.fundacionjala.coding.erikvargas;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+package org.fundacionjala.coding.erik;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * this class for  validate code ean.
  */
-public class TestEan {
+public class EanTest {
 
     private EanValidator test;
 
     /**
      * this is a builder.
      */
-    public TestEan() {
+    public EanTest() {
         test = new EanValidator();
     }
 
