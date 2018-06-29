eaa6619d077f278ba1b73a4a15b9404c0ca183dd
William Claros <will.especialista@gmail.com>
2018-06-29 15:20:35 -0400

    William Digital Root RCv2

--- src/main/java/org/fundacionjala/coding/digitalroot/DigitalRoot.java
+++ src/main/java/org/fundacionjala/coding/digitalroot/DigitalRoot.java
@@ -9,14 +9,14 @@ public class DigitalRoot {
     /**
      * Method to perform the recursive addition of the digits of a number.
      *
-     * @param numero whole inserted as parameter.
+     * @param number whole inserted as parameter.
      * @return result which is the recursive sum of a given number.
      */
-    public int digitalRoot(int numero) {
+    public int digitalRoot(int number) {
         int result = 0;
-        while (numero > 0) {
-            result = result + numero % CONSTANTE_MULTIPLICACION;
-            numero = numero / CONSTANTE_MULTIPLICACION;
+        while (number > 0) {
+            result += number % CONSTANTE_MULTIPLICACION;
+            number /= CONSTANTE_MULTIPLICACION;
         }
         return result >= CONSTANTE_MULTIPLICACION ? digitalRoot(result) : result;
     }
