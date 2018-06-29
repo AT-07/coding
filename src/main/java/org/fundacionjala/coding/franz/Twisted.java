0adfa3dd3388f724d1e4f9bc7f54e0a8d81bf3fa
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-06-28 21:51:46 -0400

    refactor code Twisted

--- src/main/java/org/fundacionjala/coding/franz/Twisted.java
+++ src/main/java/org/fundacionjala/coding/franz/Twisted.java
@@ -9,6 +9,7 @@ public class Twisted {
     private static final char THREE = '3';
     private static final char FLAG = 't';
     private static final char SEVEN = '7';
+
     /**
      * this method os a sort twisted.
      *
@@ -17,10 +18,11 @@ public class Twisted {
      */
     public Integer[] sortTwisted(final Integer[] array) {
         return Arrays.stream(array)
-                .map(number -> Integer.valueOf(number.toString()
-                        .replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
-                ).sorted().map(number -> Integer.valueOf(number.toString()
-                        .replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
-                ).toArray(Integer[]::new);
+                .map(number -> number.toString().replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
+                .map(Integer::parseInt)
+                .sorted()
+                .map(number -> number.toString().replace(THREE, FLAG).replace(SEVEN, THREE).replace(FLAG, SEVEN))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
     }
 }
