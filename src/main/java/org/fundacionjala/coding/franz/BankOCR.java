36022f83b126c8321a228b25cc605c5676148c07
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-07-02 00:19:30 -0400

    code of the BankOCR

---
+++ src/main/java/org/fundacionjala/coding/franz/BankOCR.java
@@ -0,0 +1,90 @@
+package org.fundacionjala.coding.franz;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * this class is a BankOCR.
+ */
+public class BankOCR {
+    private static final int ZERO = 0;
+    private static final int THREE = 3;
+    private static final int ELEVEN = 11;
+    private int numberMulti;
+    private static final int TEN = 10;
+
+    private static final Map<String, String> DIGITAL_NUMBERS = new HashMap<>();
+
+    static {
+        DIGITAL_NUMBERS.put("     |  |", "1");
+        DIGITAL_NUMBERS.put(" _  _||_ ", "2");
+        DIGITAL_NUMBERS.put(" _  _| _|", "3");
+        DIGITAL_NUMBERS.put("   |_|  |", "4");
+        DIGITAL_NUMBERS.put(" _ |_  _|", "5");
+        DIGITAL_NUMBERS.put(" _ |_ |_|", "6");
+        DIGITAL_NUMBERS.put(" _   |  |", "7");
+        DIGITAL_NUMBERS.put(" _ |_||_|", "8");
+        DIGITAL_NUMBERS.put(" _ |_| _|", "9");
+        DIGITAL_NUMBERS.put(" _ | ||_|", "0");
+    }
+
+    /**
+     * .
+     *
+     * @param firstLine  of account
+     * @param secondLine of account
+     * @param thirLine   of account
+     * @return number of accunt
+     */
+    public String accountNumbers(String firstLine, String secondLine, String thirLine) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(firstLine.substring(ZERO, THREE))
+                .append(secondLine.substring(ZERO, THREE))
+                .append(thirLine.substring(ZERO, THREE));
+
+        return firstLine.length() == THREE ? DIGITAL_NUMBERS.getOrDefault(builder.toString(), "?")
+                : DIGITAL_NUMBERS.getOrDefault(builder.toString(), "?")
+                .concat(accountNumbers(firstLine.substring(THREE),
+                        secondLine.substring(THREE),
+                        thirLine.substring(THREE)));
+    }
+
+    /**
+     * .
+     *
+     * @param account for validate.
+     * @return true if account is valid.
+     */
+    public boolean isValidAccount(String account) {
+        numberMulti = TEN;
+        int checksum = Arrays.stream(account.split(""))
+                .mapToInt(Integer::parseInt).map(i -> reduce(i)).sum();
+
+        return checksum % ELEVEN == 0;
+    }
+
+    /**
+     * that multiply on checksum of account.
+     *
+     * @param number is a number.
+     * @return return multiply.
+     */
+    public int reduce(int number) {
+        numberMulti -= 1;
+        return number * numberMulti;
+    }
+
+    /**
+     * .
+     *
+     * @param account is account.
+     * @return result of account.
+     */
+    public String showResults(String account) {
+        if (account.contains("?")) {
+            return String.format("%s ILL", account);
+        }
+        return isValidAccount(account) ? account : String.format("%s ERR", account);
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/franz/BankOCRTest.java
@@ -0,0 +1,135 @@
+package org.fundacionjala.coding.franz;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * this is a test of class.
+ */
+public class BankOCRTest {
+    private BankOCR bankNumber;
+
+    /**
+     * this is a method before test.
+     */
+    @Before
+    public void ini() {
+        bankNumber = new BankOCR();
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void tesyAccountNumbersNine() {
+        String lineOne = " _  _  _  _  _  _  _  _  _ ";
+        String lineTwo = "|_||_||_||_||_||_||_||_||_|";
+        String lineThr = " _| _| _| _| _| _| _| _| _|";
+        assertEquals("999999999", bankNumber.accountNumbers(lineOne, lineTwo, lineThr));
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void testAccountNumbersSeven() {
+        String lineOne = " _  _  _  _  _  _  _  _  _ ";
+        String lineTwo = "  |  |  |  |  |  |  |  |  |";
+        String lineThr = "  |  |  |  |  |  |  |  |  |";
+        assertEquals("777777777", bankNumber.accountNumbers(lineOne, lineTwo, lineThr));
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void testAccountNumbersEigth() {
+        String lineOne = " _  _  _  _  _  _  _  _  _ ";
+        String lineTwo = "|_||_||_||_||_||_||_||_||_|";
+        String lineThr = "|_||_||_||_||_||_||_||_||_|";
+        assertEquals("888888888", bankNumber.accountNumbers(lineOne, lineTwo, lineThr));
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void testAccountNumbersNumbers() {
+        String lineOne = "    _  _     _  _  _  _  _ ";
+        String lineTwo = "  | _| _||_||_ |_   ||_||_|";
+        String lineThre = "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("123456789", bankNumber.accountNumbers(lineOne, lineTwo, lineThre));
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void testAccountNumbersZero() {
+        String lineOne = " _  _  _  _  _  _  _  _  _ ";
+        String lineTwo = "| || || || || || || || || |";
+        String lineThr = "|_||_||_||_||_||_||_||_||_|";
+        assertEquals("000000000", bankNumber.accountNumbers(lineOne, lineTwo, lineThr));
+    }
+
+    /**
+     * this is a test of method accountNumber().
+     */
+    @Test
+    public void testAccountNumbersILL() {
+        String lineOne = "    _  _     _  _  _  _  _ ";
+        String lineTwo = "  | _| _||_| _ |_   ||_||_|";
+        String lineThr = "  ||_  _|  | _||_|  ||_| _ ";
+        assertEquals("1234?678?", bankNumber.accountNumbers(lineOne, lineTwo, lineThr));
+    }
+
+    /**
+     * this is a test of method isValidAccount().
+     */
+    @Test
+    public void testIsValidAccountTrue() {
+        assertTrue(bankNumber.isValidAccount("100000029"));
+    }
+
+    /**
+     * this is a test of method isValidAccount().
+     */
+    @Test
+    public void testIsValidAccountFalse() {
+        assertFalse(bankNumber.isValidAccount("123456799"));
+    }
+
+    /**
+     * this is a test of method showResult().
+     */
+    @Test
+    public void testshowResultCorrect() {
+        String actual = "457508000";
+        String expected = "457508000";
+        assertEquals(expected, bankNumber.showResults(actual));
+    }
+
+    /**
+     * this is a test of method showResult().
+     */
+    @Test
+    public void testshowResultError() {
+        String actual = "664371495";
+        String expected = "664371495 ERR";
+        assertEquals(expected, bankNumber.showResults(actual));
+    }
+
+    /**
+     * this is a test of method showResult().
+     */
+    @Test
+    public void testshowResultILL() {
+        String actual = "86110??36";
+        String expected = "86110??36 ILL";
+        assertEquals(expected, bankNumber.showResults(actual));
+    }
+}
