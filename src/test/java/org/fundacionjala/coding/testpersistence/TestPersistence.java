d11e7d9f79653986c7e45cd35a29e06cdb5eabb1
william1605 <40204267+william1605@users.noreply.github.com>
2018-07-02 09:58:08 -0400

    William Persistence (#23)
    * William Persistence
    
    * William Persistence Digital
    
    * William Persistence Digit v.3
    
    * William Persistence Digital v.5
    
    * William Persistence Digit RCv2

---
+++ src/test/java/org/fundacionjala/coding/testpersistence/TestPersistence.java
@@ -0,0 +1,63 @@
+package org.fundacionjala.coding.testpersistence;
+
+import org.fundacionjala.coding.persistence.Persistence;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * TestPersistence class that helps to run all the test cases of the Persistence class.
+ */
+public class TestPersistence {
+    private Persistence persistence;
+
+    /**
+     * Method that initializes my object persistence.
+     */
+    @Before
+    public void inicialization() {
+        persistence = new Persistence();
+    }
+
+    /**
+     * Unit Test #1 that verifies the correctness of the persistence method.
+     */
+    @Test
+    public void numberPersistentTest1Correct() {
+        final int persistenceDigit = 39;
+        final int expectResult = 3;
+        assertEquals(expectResult, persistence.persistenceDigit(persistenceDigit));
+    }
+
+    /**
+     * Unit Test #2 that verifies the correctness of the persistence method.
+     */
+    @Test
+    public void numberPersistentTest2Correct() {
+        final int persistenceDigit = 999;
+        final int expectResult = 4;
+        assertEquals(expectResult, persistence.persistenceDigit(persistenceDigit));
+
+    }
+
+    /**
+     * Unit Test #3 that verifies the correctness of the persistence method.
+     */
+    @Test
+    public void numberPersistentTest3Correct() {
+        final int persistenceDigit = 1;
+        assertEquals(0, persistence.persistenceDigit(persistenceDigit));
+    }
+
+    /**
+     * Unit Test #4 that verifies the correctness of the persistence method with erroneous values.
+     */
+    @Test
+    public void numberPersistentTestErroneous() {
+        final int persistenceDigit = 34567;
+        final int expectResult = 4;
+        assertNotEquals(expectResult, persistence.persistenceDigit(persistenceDigit));
+    }
+}
