af5cc5ab7dde3e16df35bd4a77f9881c51ee3760
ketty01 <72768163ketty@gmail.com>
2018-07-01 23:52:31 -0400

    ketty spin words

---
+++ src/main/java/org/fundacionjala/coding/ketty/SpinWords.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.coding.ketty;
+
+/**
+ * this is class verify if a string is a SPIN WORD.
+ */
+
+public class SpinWords {
+
+    private static final int LIMIT_LETTER = 5;
+
+    /**
+     * @param phrase it method for verify if a string can to be a Spin Word.
+     * @return a string.
+     */
+
+    public String getSpinWord(String phrase) {
+        StringBuilder newPhrase = new StringBuilder();
+        String[] word = phrase.split(" ");
+
+
+        for (String wordPart : word) {
+            if (wordPart.length() < LIMIT_LETTER) {
+                newPhrase.append(" " + wordPart);
+
+            } else {
+
+                StringBuilder reverse = new StringBuilder(wordPart);
+                newPhrase.append(" " + reverse.reverse().toString());
+            }
+        }
+        return newPhrase.toString().trim();
+
+    }
+
+
+}
---
+++ src/test/java/org/fundacionjala/coding/ketty/SpinWordsTest.java
@@ -0,0 +1,52 @@
+package org.fundacionjala.coding.ketty;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * class SpinWordsTest it is test verify.
+ */
+
+public class SpinWordsTest {
+    private SpinWords spinWord;
+    private static final String PHRASE_N = "hey fellow warriors";
+    private static final String PHRASE_CORRECTLY = "hey wollef sroirraw";
+    private static final String PHASE_FALSE = "hey wollef warriors";
+
+    /**
+     * this is method execute firth.
+     */
+    @Before
+    public void before() {
+
+        spinWord = new SpinWords();
+    }
+
+    /**
+     * it test is verify that a string is Spin Word.
+     */
+    @Test
+    public void testGetSprintWordPass() {
+
+
+        assertTrue(PHRASE_CORRECTLY.equals(spinWord.getSpinWord(PHRASE_N)));
+
+
+    }
+
+    /**
+     * it test verify that a string add is not Spin Word.
+     */
+
+    @Test
+    public void testGetSprintWordFailed() {
+
+        assertFalse(PHASE_FALSE.equals(spinWord.getSpinWord(PHRASE_N)));
+
+
+    }
+
+}
