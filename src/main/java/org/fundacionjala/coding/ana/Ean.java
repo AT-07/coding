d4e9feb9dda88551afcd78749a9d2efae0bb6de9
lluviana <ana.mzenteno@gmail.com>
2018-07-02 18:36:36 -0400

    ana ean

--- src/main/java/org/fundacionjala/coding/ana/Ean.java
+++ src/main/java/org/fundacionjala/coding/ana/Ean.java
@@ -6,25 +6,21 @@ package org.fundacionjala.coding.ana;
  * @author Ana Maria Mamani Zenteno.
  */
 public class Ean {
-    private static final int ONE = 1;
     private static final int THREE = 3;
     private static final int DIVIDABLE = 10;
 
     /**
-     * suma de comprobacion de 12 digitos.
+     * sum of check of  12 digit.
      *
-     * @param number numero esperodo.
-     * @return devuelve un valor boolean.
+     * @param number value of entry.
+     * @return a value boolean.
      */
     public boolean articleNumber(final String number) {
         int sumValue = 0;
         int ultimateValue = Integer.parseInt(number.substring(number.length() - 1));
         for (int n = 0; n < number.length() - 1; n++) {
-            if (n % 2 == 0) {
-                sumValue += Character.getNumericValue(number.charAt(n)) * ONE;
-            } else {
-                sumValue += Character.getNumericValue(number.charAt(n)) * THREE;
-            }
+            int data = Character.getNumericValue(number.charAt(n));
+            sumValue += n % 2 == 0 ? data : data * THREE;
         }
         sumValue = sumValue % DIVIDABLE == 0 ? 0 : DIVIDABLE - (sumValue % DIVIDABLE);
         return sumValue == ultimateValue;
