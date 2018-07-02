b7b587e7a4e33be58c789678d884950039abad92
yerel <estalinyerel123@gmail.com>
2018-07-02 10:09:13 -0400

    Estalin StopSpinWords (#12)
    * Estalin StopSpinWords
    
    * refactor
    
    * modify
    
    * refactoring code

---
+++ src/main/java/org/fundacionjala/coding/yerel/StopSpinWords.java
@@ -0,0 +1,26 @@
+package org.fundacionjala.coding.yerel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * this class convert word long is major five of sentence.
+ */
+public class StopSpinWords {
+
+    private static final int LENGTWORD_COVERT = 4;
+    private static final String SPACE = " ";
+
+    /**
+     * @param sentence is a word to invert.
+     * @return word invert.
+     */
+    public String spinWords(final String sentence) {
+        String[] wordsSepare = sentence.split(SPACE);
+        List<String> wordArray = new ArrayList<>();
+        for (String word: wordsSepare) {
+            wordArray.add(word.length() > LENGTWORD_COVERT ? new StringBuilder(word).reverse().toString() : word);
+        }
+        return String.join(SPACE, wordArray);
+    }
+}
