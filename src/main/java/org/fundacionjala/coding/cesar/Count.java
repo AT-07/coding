495667dea839d78b0927bf49844d4a96f5ef7069
cesar calvi <ccalvilujan@gmail.com>
2018-07-03 17:20:52 -0600

    CESAR COUNTCHAR

---
+++ src/main/java/org/fundacionjala/coding/cesar/Count.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.cesar;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author admin-hp
+ */
+public class Count {
+    /**
+     * @param cadena String precess for count character.
+     * @param chr    what Character that is counted.
+     * @return cont to return.
+     */
+    public int countChar(final String cadena, final String chr) {
+        List<String> lst = Arrays.asList(cadena.split(""));
+        int upper = Collections.frequency(lst, chr.toLowerCase());
+        int lower = Collections.frequency(lst, chr.toUpperCase());
+        int cont = upper + lower;
+        return cont;
+    }
+}
