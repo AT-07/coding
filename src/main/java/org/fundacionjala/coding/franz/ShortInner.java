325cdd32a47d500ca97411b1af17e4eb6d09cf30
William Claros <will.especialista@gmail.com>
2018-07-03 15:37:05 -0400

    William Sort Inner Content

--- src/main/java/org/fundacionjala/coding/franz/ShortInner.java
+++ src/main/java/org/fundacionjala/coding/franz/ShortInner.java
@@ -16,11 +16,8 @@ public class ShortInner {
      * @param words is the words to invert
      * @return return words inner inverted
      */
-<<<<<<< Updated upstream
+
     String sortTheInnerContent(final String words) {
-=======
-    public String sortTheInnerContent(String words) {
->>>>>>> Stashed changes
         String[] listWords = words.split(" ");
         StringJoiner join = new StringJoiner(" ");
         String[] listChars;
