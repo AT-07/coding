37a8b5e590c2a3c31118cb379c3949eadab0e57d
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-07-03 19:26:23 -0400

    franz evaporator code

---
+++ src/main/java/org/fundacionjala/coding/franz/Evaporator.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.coding.franz;
+
+/**
+ * this is class of Evaporator.
+ */
+public class Evaporator {
+    private static final int HUNDRED = 100;
+
+    /**
+     * this method.
+     *
+     * @param content      cant
+     * @param evapDay cant of evaporate
+     * @param threshold    limit porcent
+     * @return days that evaporate
+     */
+    public int evaporator(double content,double evapDay,double threshold) {
+        double limit = content * (threshold / HUNDRED);
+        int days = 0;
+        double reduce;
+        while (content > limit) {
+            reduce = content * (evapDay / HUNDRED);
+            content -= reduce;
+            days++;
+        }
+        return days;
+    }
+}
