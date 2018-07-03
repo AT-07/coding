56866a78560a38bfd1f221c3988ba8fe6d12c547
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-07-03 19:35:02 -0400

    added final to parameter

--- src/main/java/org/fundacionjala/coding/franz/CountCharacters.java
+++ src/main/java/org/fundacionjala/coding/franz/CountCharacters.java
@@ -12,13 +12,13 @@ public class CountCharacters {
      * @param c   charcater of reference.
      * @return cant of characters
      */
-    public int charCount(String str, char c) {
+    public int charCount(final String str, final char c) {
         int count = 0;
+        String charac = String.valueOf(c);
         for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i, i + 1).equalsIgnoreCase("" + c)) {
+            if (str.substring(i, i + 1).equalsIgnoreCase(charac)) {
                 count++;
             }
-
         }
         return count;
     }
