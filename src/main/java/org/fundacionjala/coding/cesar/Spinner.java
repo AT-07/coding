36244f10212aec999fefa047bf004b22b69cbe6a
cesar calvi <ccalvilujan@gmail.com>
2018-07-02 19:13:05 -0600

    CESAR SPINWORDS

--- src/main/java/org/fundacionjala/coding/cesar/Spinner.java
+++ src/main/java/org/fundacionjala/coding/cesar/Spinner.java
@@ -5,20 +5,19 @@ package org.fundacionjala.coding.cesar;
  * @author admin-hp
  */
 public class Spinner {
-    private static String delimiterWhiteSpace = " ";
-
+    private static final String DELIMITER_WHITE_SPACE = " ";
+    private static final int CONSTANCE = 4;
     /**
      * @param totalText string that will be introduced.
      * @return string en reverse.
      */
     public String spinWords(final String totalText) {
-        final int constante = 4;
-        String[] resWords = totalText.split(delimiterWhiteSpace);
+        String[] resWords = totalText.split(DELIMITER_WHITE_SPACE);
         for (int i = 0; i < resWords.length; i++) {
-            if (resWords[i].length() > constante) {
+            if (resWords[i].length() > CONSTANCE) {
                 resWords[i] = new StringBuffer(resWords[i]).reverse().toString();
             }
         }
-        return String.join(delimiterWhiteSpace, resWords);
+        return String.join(DELIMITER_WHITE_SPACE, resWords);
     }
 }
