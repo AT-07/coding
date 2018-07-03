dfa6dc9161d9d9cd21449310b190d80dfc6eb577
lluviana <ana.mzenteno@gmail.com>
2018-07-03 13:24:11 -0400

    ana dna

--- src/main/java/org/fundacionjala/coding/ana/DnaStrand.java
+++ src/main/java/org/fundacionjala/coding/ana/DnaStrand.java
@@ -8,21 +8,26 @@ import java.util.HashMap;
  * @author Ana Maria Mamani Zenteno.
  */
 public class DnaStrand {
+    private static final HashMap<Character, Character> COMPLEMENTDNA = new HashMap<>();
+
+    static {
+        COMPLEMENTDNA.put('C', 'G');
+        COMPLEMENTDNA.put('G', 'C');
+        COMPLEMENTDNA.put('T', 'A');
+        COMPLEMENTDNA.put('A', 'T');
+    }
+
     /**
      * method for the complement of DNA.
      *
      * @param adn string of into.
-     * @return  a string.
+     * @return a string.
      */
     public String makeComplement(final String adn) {
+
         StringBuilder getComplement = new StringBuilder();
-        HashMap<Character, Character> complementDna = new HashMap<>();
-        complementDna.put('C', 'G');
-        complementDna.put('G', 'C');
-        complementDna.put('T', 'A');
-        complementDna.put('A', 'T');
         for (int i = 0; i < adn.length(); i++) {
-            getComplement.append(String.valueOf(complementDna.get(adn.charAt(i))));
+            getComplement.append(String.valueOf(COMPLEMENTDNA.get(adn.charAt(i))));
         }
         return getComplement.toString();
     }
