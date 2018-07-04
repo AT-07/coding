bab019af5cb2dd09891dd394046666f4346d8784
ketty01 <40204344+ketty01@users.noreply.github.com>
2018-07-04 13:18:54 -0400

    countChar ketty (#86)
    * countChar ketty
    
    * fixed countChart ketty
    
    * ketty
    
    * ketty01
    
    * Count Char ketty

---
+++ src/test/java/org/fundacionjala/coding/ketty/CountCharTest.java
@@ -0,0 +1,73 @@
+package org.fundacionjala.coding.ketty;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * test of CountChar.
+ */
+
+public class CountCharTest {
+    private CountChar countChar;
+
+    /**
+     * before firth.
+     */
+    @Before
+    public void before() {
+        countChar = new CountChar();
+    }
+
+    /**
+     * test of verify that pass.
+     */
+
+    @Test
+    public void countChartPass() {
+        final String phrase = "fizzbuzz";
+        final char letter = 'z';
+        final int expectedResult = 4;
+
+        assertEquals(expectedResult, countChar.countChar(phrase, letter));
+    }
+
+    /**
+     * test in case the pass a count phrase.
+     */
+    @Test
+    public void countChartPass2() {
+
+        final String phrase = "Fancy fifth fly aloof";
+        final char letter1 = 'f';
+        final int expectedResult = 5;
+
+        assertEquals(expectedResult, countChar.countChar(phrase, letter1));
+    }
+
+    /**
+     * test it is verify that string phrase and letter failed.
+     */
+    @Test
+    public void countChartFailed() {
+        final String phrase = "fizzbuzz";
+        final char letter = 'f';
+        final int expected = 6;
+        assertNotEquals(expected, countChar.countChar(phrase, letter));
+    }
+
+    /**
+     * test it is verify that string phrase and letter failed.
+     */
+    @Test
+    public void countChartFailed1() {
+        final String phrase = "Fancy fifth fly aloof";
+        final char letter = 'f';
+        final int expected = 9;
+        assertNotEquals(expected, countChar.countChar(phrase, letter));
+    }
+
+
+}
