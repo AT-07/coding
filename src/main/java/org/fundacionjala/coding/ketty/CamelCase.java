da661d477ee4e160353200ee5713fa0d84253702
ketty01 <72768163ketty@gmail.com>
2018-07-04 03:06:57 -0400

    Camel Case Ketty

---
+++ src/main/java/org/fundacionjala/coding/ketty/CamelCase.java
@@ -0,0 +1,24 @@
+package org.fundacionjala.coding.ketty;
+
+import java.util.StringJoiner;
+
+/**
+ * @author ketty
+ * class CamlCase of a string.
+ */
+
+public class CamelCase {
+    /**
+     * @param phrase it is string :
+     * @return count.
+     */
+    public String camelCase(String phrase) {
+        StringJoiner union = new StringJoiner("");
+        String[] word = phrase.split(" ");
+        for (String chartLetter : word) {
+            union.add(Character.toUpperCase(chartLetter.charAt(0)) + chartLetter.substring(1));
+
+        }
+        return union.toString().trim();
+    }
+}
