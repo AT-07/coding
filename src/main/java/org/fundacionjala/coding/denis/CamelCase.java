d21c7b05bc86e3f8d65bb450beb44658a72e34dc
Denis Camacho <denis_cc1729@hotmail.com>
2018-07-06 20:02:22 -0400

    denis CamaelCase mejorado

--- src/main/java/org/fundacionjala/coding/denis/CamelCase.java
+++ src/main/java/org/fundacionjala/coding/denis/CamelCase.java
@@ -8,24 +8,22 @@ import java.util.StringJoiner;
 public class CamelCase {
     private static final int POSZERO = 0;
     private static final int POSONE = 1;
-    private static final int SIZE = 2;
 
     /**
-     *
      * @param str is the data with a work.
      * @return string in the work.
      */
     public String camelCase(final String str) {
-        if (str.length() < SIZE) {
-            return str.length() == POSZERO ? str : str.toUpperCase();
+        if (str.length() == POSZERO) {
+            return str;
         }
         StringJoiner spectedResult = new StringJoiner("");
 
         for (String word : str.split(" ")) {
-            if (!word.equals("")) {
-                spectedResult.add(word.substring(POSZERO, POSONE)
-                        .toUpperCase().concat(word.substring(POSONE)));
-            }
+             if (!word.equals("")) {
+            spectedResult.add(word.substring(POSZERO, POSONE)
+                    .toUpperCase().concat(word.substring(POSONE)));
+              }
         }
         return spectedResult.toString();
     }
