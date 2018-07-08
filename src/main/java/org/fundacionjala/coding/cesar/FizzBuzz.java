cada82b67909435b6a195f083c156c8a076b8b3f
cesar calvi <ccalvilujan@gmail.com>
2018-07-08 00:12:59 -0600

    CESAR FIZZBUZZ

--- src/main/java/org/fundacionjala/coding/cesar/FizzBuzz.java
+++ src/main/java/org/fundacionjala/coding/cesar/FizzBuzz.java
@@ -9,24 +9,43 @@ import java.util.stream.Collectors;
  */
 public class FizzBuzz {
     private static final int FIVE = 5;
-    private static final int TREE = 3;
+    private static final int THREE = 3;
     /**
      * @param range List from int.
      * @return list String replace.
      */
     public List<String> doBuzzFizz(final List<Integer> range) {
 
-        return range.stream().map(i -> i % FIVE == 0 & i % TREE == 0
-                ? "fizzbuzz"
+        List<String> lst = range
+                .stream()
+                .map(i -> (Integer.toString(i)))
+                .collect(Collectors.toList());
+        return lst.stream()
+                .map(i ->
+                        this.isStringNumber(i) == true && Integer.parseInt(i)
+                                % FIVE == 0 & Integer.parseInt(i) % THREE == 0 ? "fizzbuzz" : i
+                )
+                .map(i ->
+                        this.isStringNumber(i) == true && Integer.parseInt(i)
+                                % FIVE == 0 ? "buzz" : i
+                )
+                .map(i ->
+                        this.isStringNumber(i) == true && Integer.parseInt(i) % THREE == 0 ? "fizz" : i
+                )
+                .collect(Collectors.toList());
+    }
 
-                : i % FIVE == 0
-                ? "buzz"
-
-                : i % TREE == 0
-                ? "fizz"
-
-                : Integer.toString(i)
-        ).collect(Collectors.toList());
-
+    /**
+     *
+     * @param e value for evaluation.
+     * @return value true or false.
+     */
+    public boolean isStringNumber(final String e) {
+        try {
+            int i = Integer.parseInt(e);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 }
