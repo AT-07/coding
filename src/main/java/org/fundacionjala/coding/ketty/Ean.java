6dc12641c2c890876a282583fd5d08c98a33e784
ketty01 <72768163ketty@gmail.com>
2018-07-08 19:43:50 -0400

    Ketty EAN

---
+++ src/main/java/org/fundacionjala/coding/ketty/Ean.java
@@ -0,0 +1,37 @@
+package org.fundacionjala.coding.ketty;
+
+/**
+ * @author ketty camcho Vasquez.
+ */
+
+public class Ean {
+    /**
+     * El  eanOperation, verify if is  string number is "EAN".
+     *
+     * @param num is the is a number string.
+     * @return a boolean,  true if the String number is "EAN" and false when it is not an "EAN".
+     */
+    public boolean eanOperation(final String num) {
+        final int oodNumber = 1;
+        final int evenNumber = 3;
+        final int ten = 10;
+        int index = 1;
+        int sum = 0;
+        String[] newNumber = num.split("");
+        for (int i = 0; i < newNumber.length - 1; i++) {
+            if (index % 2 == 0) {
+                sum += Integer.parseInt(newNumber[i]) * evenNumber;
+                index++;
+            } else {
+                sum += Integer.parseInt(newNumber[i]) * oodNumber;
+
+                index++;
+            }
+        }
+
+        int x = Integer.parseInt(newNumber[newNumber.length - 1]);
+
+
+        return x == ten - (sum % ten);
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/ketty/EanTest.java
@@ -0,0 +1,37 @@
+package org.fundacionjala.coding.ketty;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * test of Ean class.
+ */
+
+public class EanTest {
+
+    private Ean ean = new Ean();
+
+    /**
+     * test validate 400330101839 is true.
+     */
+    @Test
+    public void eanOperationPass() {
+        String num = "4003301018398";
+        assertTrue(ean.eanOperation(num));
+
+    }
+
+    /**
+     * test validate   string  "4003301018392" its last digit 2 is False.
+     */
+    @Test
+    public void eanOperationFailed() {
+
+        String num = "4003301018392";
+        assertFalse(ean.eanOperation(num));
+    }
+
+
+}
