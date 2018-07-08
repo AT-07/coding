bb4035f2a5279c02b02b2d72cd20d0eb129ce296
William Claros <will.especialista@gmail.com>
2018-07-08 01:30:00 -0400

    William Fizz Buzz

---
+++ src/main/java/org/fundacionjala/coding/william/FizzBuzz.java
@@ -0,0 +1,38 @@
+package org.fundacionjala.coding.william;
+
+import java.util.StringJoiner;
+
+/**
+ * FizzBuzz class that exchanges the values of digits 3 and 5.
+ */
+public class FizzBuzz {
+
+    private static final int NUMBERTHREE = 3;
+    private static final int NUMBERFIVE = 5;
+    private static final int NUMBERTEN = 10;
+    private static final int NUMBERZERO = 0;
+
+    /**
+     * Method that replaces the values 3 and 5 in a chain.
+     *
+     * @param number is the number where the chain will arrive in numbers.
+     * @return returns a string with the numbers exchanged.
+     */
+    public String fizzBuzz(int number) {
+        StringJoiner result = new StringJoiner(" ");
+        for (int i = 1; i <= number; i++) {
+            StringBuilder variable = new StringBuilder(String.valueOf(i));
+            if (i % NUMBERTHREE != NUMBERZERO && i % NUMBERFIVE != NUMBERZERO && !variable.toString().contains("3")
+                    && !variable.toString().contains("5")) {
+                result.add(String.valueOf(i));
+            }
+            if (i % NUMBERTHREE == NUMBERZERO || i / NUMBERTEN == NUMBERTHREE || variable.toString().contains("3")) {
+                result.add("FIZZ");
+            }
+            if (i % NUMBERFIVE == NUMBERZERO || i / NUMBERTEN == NUMBERFIVE || variable.toString().contains("5")) {
+                result.add("BUZZ");
+            }
+        }
+        return result.toString();
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/william/TestFizzBuzz.java
@@ -0,0 +1,64 @@
+package org.fundacionjala.coding.william;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Class that runs all my tests to verify the correctness of my FizzBuzz class.
+ */
+public class TestFizzBuzz {
+
+    private FizzBuzz number;
+
+    /**
+     * Test to initialize my object number.
+     */
+    @Before
+    public void test() {
+        number = new FizzBuzz();
+    }
+
+    /**
+     * Test to validate the correctness of my test method fizzBuzz.
+     */
+    @Test
+    public void fizzBuzzTestCorrect() {
+        final int actualValue = 27;
+        final String expectedResult = "1 2 FIZZ 4 BUZZ FIZZ 7 8 FIZZ BUZZ 11 FIZZ FIZZ 14 FIZZ BUZZ 16 17 FIZZ 19"
+                + " BUZZ FIZZ 22 FIZZ FIZZ BUZZ 26 FIZZ";
+        assertEquals(expectedResult, number.fizzBuzz(actualValue));
+    }
+
+    /**
+     * Test to validate the correctness of my test method fizzBuzz.
+     */
+    @Test
+    public void fizzBuzzTest2Correct() {
+        final int actualValue = 12;
+        final String expectedResult = "1 2 FIZZ 4 BUZZ FIZZ 7 8 FIZZ BUZZ 11 FIZZ";
+        assertEquals(expectedResult, number.fizzBuzz(actualValue));
+    }
+
+    /**
+     * Test to validate the correctness of my test method fizzBuzz.
+     */
+    @Test
+    public void fizzBuzzTest3Correct() {
+        final int actualValue = 5;
+        final String expectedResult = "1 2 FIZZ 4 BUZZ";
+        assertEquals(expectedResult, number.fizzBuzz(actualValue));
+    }
+
+    /**
+     * Test to validate the correctness of my fizzBuzz method test with an erroneous expected result.
+     */
+    @Test
+    public void fizzBuzzTestIncorrect() {
+        final int actualValue = 6;
+        final String expectedResult = "1 2 FIZZ 4 BUZZ 6";
+        assertNotEquals(expectedResult, number.fizzBuzz(actualValue));
+    }
+}
