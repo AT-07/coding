9d23f8abc48c8203e898bb287bb949938949df49
yerel <estalinyerel123@gmail.com>
2018-07-08 17:53:12 -0400

    refactor

--- src/main/java/org/fundacionjala/coding/yerel/ExcerciseCountChar.java
+++ src/main/java/org/fundacionjala/coding/yerel/ExcerciseCountChar.java
@@ -7,13 +7,13 @@ public class ExcerciseCountChar {
 
     /**
      * @param sentence is a sentecense to count.
-     * @param leter    it is a specific leter find.
+     * @param letter    it is a specific letter find.
      * @return a number cuantity.
      */
-    public int countChar(final String sentence, final char leter) {
+    public int countChar(final String sentence, final char letter) {
         int count = 0;
         for (int i = 0; i < sentence.length(); i++) {
-            if (String.valueOf(sentence.charAt(i)).equalsIgnoreCase(String.valueOf(leter))) {
+            if (Character.toLowerCase(sentence.charAt(i)) == letter) {
                 count++;
             }
         }
