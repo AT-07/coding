59c69a70c273d804bbcf42a8a9c8a23da8b63f17
ketty01 <40204344+ketty01@users.noreply.github.com>
2018-07-09 12:04:07 -0400

    class twisted fixed (#76)
    * class twisted fixed
    
    * ketty TWISTED
    
    * twisted ketty camacho
    
    * Twisted ketty
    
    * Twisted ketty Camacho
    
    * TWISTED ketty

---
+++ src/main/java/org/fundacionjala/coding/ketty/Twisted.java
@@ -0,0 +1,40 @@
+package org.fundacionjala.coding.ketty;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * class twisted.
+ */
+public class Twisted {
+
+    private static final Map<Integer, Integer> REPLACE = new HashMap<>();
+    private static final int THREE = 3;
+    private static final int SEVEN = 7;
+
+    static {
+
+        REPLACE.put(THREE, SEVEN);
+        REPLACE.put(SEVEN, THREE);
+    }
+
+    /**
+     * method twisted.
+     *
+     * @param arrayInt is an array of integer.
+     * @return int [] it is a array the return the solution.
+     */
+
+    public int[] twisted(final int[] arrayInt) {
+
+
+        int[] arrayNew = new int[arrayInt.length];
+        Arrays.sort(arrayInt);
+        for (int i = 0; i <= arrayInt.length - 1; i++) {
+            arrayNew[i] = arrayInt[i] == THREE || arrayInt[i] == SEVEN ? REPLACE.get(arrayInt[i]) : arrayInt[i];
+
+        }
+        return arrayNew;
+    }
+}
