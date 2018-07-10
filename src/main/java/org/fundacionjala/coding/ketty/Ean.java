a747bb54bde92aa66e89f24f0b242a4889e55e05
KETTY CAMACHO <72768163ketty@gmail.com>
2018-07-10 10:41:49 -0400

    EAN KETTY CV

--- src/main/java/org/fundacionjala/coding/ketty/Ean.java
+++ src/main/java/org/fundacionjala/coding/ketty/Ean.java
@@ -12,21 +12,13 @@ public class Ean {
      * @return a boolean,  true if the String number is "EAN" and false when it is not an "EAN".
      */
     public boolean eanOperation(final String num) {
-        final int oodNumber = 1;
         final int evenNumber = 3;
         final int ten = 10;
-        int index = 1;
         int sum = 0;
         String[] newNumber = num.split("");
         for (int i = 0; i < newNumber.length - 1; i++) {
-            if (index % 2 == 0) {
-                sum += Integer.parseInt(newNumber[i]) * evenNumber;
-                index++;
-            } else {
-                sum += Integer.parseInt(newNumber[i]) * oodNumber;
 
-                index++;
-            }
+            sum += (i + 1) % 2 == 0 ? Integer.parseInt(newNumber[i]) * evenNumber : Integer.parseInt(newNumber[i]);
         }
 
         int x = Integer.parseInt(newNumber[newNumber.length - 1]);
