f5956fa746e36395a45c75a316061febc4ece9c3
denis1729 <40204046+denis1729@users.noreply.github.com>
2018-07-10 16:13:27 -0400

    denis FizzBuzz (#113)
    * denis FizzBuzz
    
    * denis FizzBuzz mejorado
    
    * denis FizzBuzz mejorado
    
    * denis fizzBuzz beta
    
    * denis fizzBuzz RC

---
+++ src/main/java/org/fundacionjala/coding/denis/FizzBuzz.java
@@ -0,0 +1,46 @@
+package org.fundacionjala.coding.denis;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * . this is the class principal of FizzBuzz.
+ */
+public class FizzBuzz {
+    private static final int FIZZCONT = 3;
+    private static final int BUZZCONT = 5;
+
+    /**
+     * @param numbers is the data work.
+     * @return the spected result.
+     */
+    public String fizzBuzzCheck(final List<Integer> numbers) {
+        StringJoiner espectedResult = new StringJoiner(",");
+        for (int res : numbers) {
+            espectedResult.add(verifyFizzBuzz(res));
+        }
+        return espectedResult.toString();
+    }
+
+    /**
+     * @param res is the date with the work.
+     * @return of the string res.
+     */
+    private String verifyFizzBuzz(int res) {
+        final String data = String.valueOf(res);
+        boolean dataFizz = data.contains("3");
+        boolean dataBuzz = data.contains("5");
+
+        if (res % FIZZCONT == 0 && res % BUZZCONT == 0) {
+            return "FizzBuzz";
+        }
+        if (res % BUZZCONT == 0 || dataBuzz) {
+            return "Buzz";
+        }
+        if (res % FIZZCONT == 0 || dataFizz) {
+            return "Fizz";
+        }
+
+        return data;
+    }
+}
