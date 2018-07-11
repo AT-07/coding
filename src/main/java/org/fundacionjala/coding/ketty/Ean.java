14087a1bbb6d02e90866ef4819b71411ff44e40b
KETTY CAMACHO <72768163ketty@gmail.com>
2018-07-10 22:53:06 -0400

    EAn kcv

--- src/main/java/org/fundacionjala/coding/ketty/Ean.java
+++ src/main/java/org/fundacionjala/coding/ketty/Ean.java
@@ -5,6 +5,10 @@ package org.fundacionjala.coding.ketty;
  */
 
 public class Ean {
+
+    private static final int POSITION_PAR = 3;
+    private static final int POSITION_IMPAR = 1;
+
     /**
      * El  eanOperation, verify if is  string number is "EAN".
      *
@@ -12,20 +16,29 @@ public class Ean {
      * @return a boolean,  true if the String number is "EAN" and false when it is not an "EAN".
      */
     public boolean eanOperation(final String num) {
-        final int evenNumber = 3;
+
         final int ten = 10;
         int sum = 0;
-        String[] newNumber = num.split("");
-        for (int i = 0; i < newNumber.length - 1; i++) {
 
-            sum += (i + 1) % 2 == 0 ? Integer.parseInt(newNumber[i]) * evenNumber : Integer.parseInt(newNumber[i]);
+        String[] numberArray = num.split("");
+
+        for (int i = 0; i <= numberArray.length - 2; i++) {
+
+            sum += getEANNumber(Integer.parseInt(numberArray[i]), i + 1);
         }
 
-        int x = Integer.parseInt(newNumber[newNumber.length - 1]);
+        int value = (sum % ten) == 0 ? 0 : ten - (sum % ten);
 
-        int answere = sum % ten == 0 ? 0 : ten - (sum % ten);
+        return Integer.parseInt(numberArray[numberArray.length - 1]) == value;
+    }
 
+    /**
+     * @param number it is  String number.
+     * @param index  value.
+     * @return a value int.
+     */
 
-        return x == answere;
+    public int getEANNumber(int number, int index) {
+        return index % 2 == 0 ? number * POSITION_PAR : number * POSITION_IMPAR;
     }
 }
